ccba0fac231cb2056982853993c07fb0bf059fe8
vapostol <apostolou.vicky@gmail.com>
2025-12-03 14:51:51 +0200

    fix: Match gender values to backend validation
    Change gender dropdown values from lowercase to capitalized
    to match backend validation (Male, Female, Other)

--- client/js/views/RegisterView.js
+++ client/js/views/RegisterView.js
@@ -87,10 +87,9 @@ export default {
                                 <label for="gender">Gender *</label>
                                 <select id="gender" name="gender" required>
                                     <option value="">Select...</option>
-                                    <option value="male">Male</option>
-                                    <option value="female">Female</option>
-                                    <option value="other">Other</option>
-                                    <option value="prefer-not-to-say">Prefer not to say</option>
+                                    <option value="Male">Male</option>
+                                    <option value="Female">Female</option>
+                                    <option value="Other">Other</option>
                                 </select>
                             </div>
                         </div>
